11da06853e15f5b1ab049d9804a372cba94f5832
aymanone <aymanone@users.noreply.github.com>
2025-09-29 13:24:20 +0300

    adding to gitignore some folders

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 //import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
